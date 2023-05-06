41e85b653ac0db1296e982713b0aca9dad999099
Bubblecrown <suchanya.namwong1@gmail.com>
2023-05-07 02:45:48 +0700

    add env file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from 'path';
+import { resolve } from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +9,8 @@ export default defineConfig({
     port: 3000,
   },
   define: {
-    'process.env': process.env  },
+    "process.env": process.env,
+  },
   esbuild: {
     loader: "jsx",
   },
@@ -22,7 +24,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      "@": resolve(__dirname, "src"),
     },
   },
 });
